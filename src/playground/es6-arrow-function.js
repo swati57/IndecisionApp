5f255eaccc5a74130c88e836fb4d88e0610224bc
swati57 <swatiragarwal57@gmail.com>
2024-02-24 11:52:42 +0530

    First commit

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,12 @@
+const square = function(x){
+    return x*x;
+}
+//no fun name
+const squareArrow = (x) => {return x*x};
+const squareArrow1 = (x) => x*x;
+console.log(squareArrow1(4));
+
+// var name='Swati Agarwal';
+const getfirstName = (name) => name.split(' ')[0];
+
+console.log(getfirstName('Swati Agarwal'));
